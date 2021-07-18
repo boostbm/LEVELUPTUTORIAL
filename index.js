18c820f2169cae4f3f272ff0fe3351ba1db73850
Boost <boostbm@gmail.com>
2021-07-18 15:55:21 -0700

    Updated JS

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const logo = document.getElementById("logo");
+// Generate Random Color
+// RGB 0-255 
+// Apply to Dom
+// Update Color on Event. 
+
+
+const generateColorValue = () => Math.floor(Math.random() * 256);
+
+const createColor = () => {
+  const red = generateColorValue();
+  const green = generateColorValue();
+  const blue = generateColorValue();
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+const applyColorToBody = () => {
+  const color = createColor;
+  document.body.style.backgroundColor = color;
+}
+
+console.log(applyColorToBody());
