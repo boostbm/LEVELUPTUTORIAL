f902a08e1cbb40ef30e2458584259ebe8c2cc0df
Boost <boostbm@gmail.com>
2021-07-22 16:55:37 -0700

    Updated JS

--- index.js
+++ index.js
@@ -90,9 +90,35 @@ const shoppingCart = [
 // console.log(simpleShoppingCart);
 // console.log(total);
 
-const total = simpleShoppingCart.reduce((total, currentPrice) => {
-  return total = currentPrice;
-})
+// const total = simpleShoppingCart.reduce((total, currentPrice) => {
+//   return total = currentPrice;
+// })
 
-console.log([...simpleShoppingCart].reverse());
-console.log(simpleShoppingCart);
+// console.log([...simpleShoppingCart].reverse());
+// console.log(simpleShoppingCart);
+
+
+console.log(this);
+
+const test = {
+  testFunc: function() {
+    console.log(this);
+  },
+  testFuncTwo: () => {
+    console.log(this);
+  }
+};
+
+function team() {
+  console.log(this);
+}
+
+const button = document.getElementById("new-colors");
+button.addEventListener("click", function() {
+  console.log(this);
+});
+
+// test.testFunc();
+// test.testFuncTwo();
+// team();
+// var test = new team();
